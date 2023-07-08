1b50103031f395b2b3b405c6edc9b80858c77d16
MitchellJustinMSantamena <138697740+MitchellJustinMSantamena@users.noreply.github.com>
2023-07-08 13:02:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,18 @@
-const NFTs = [
-    {
-        name: "Nike Air Max 1",
-        size: "10",
-        properties: "Leather",
-        color: "White",
-        brand: "Nike"
-    },
-    {
-        name: "Adidas Superstar",
-        size: "11",
-        properties: "Canvas",
-        color: "Black",
-        brand: "Adidas"
-    },
-    {
-        name: "Jordan 1",
-        size: "12",
-        properties: "Suede",
-        color: "Red",
-        brand: "Jordan"
-    }
-  ];
+let nftArray = [];
+
+function mintNFT(brand, collection, name, description) {
  
-  function mintNFT(nft) {
-    NFTs.push(nft);
-    return nft;
-  }
- 
-  function listNFTs() {
-    for (let i = 0; i < NFTs.length; i++) {
-        console.log(NFTs[i]);
-    }
-    console.log("------------------");
-  }
- 
-  function getTotalSupply() {
-    console.log(`Total Supply: ${NFTs.length}`);
-    console.log("------------------");
-  }
- 
-  // Call your functions below this line
- 
-  listNFTs();
- 
-  mintNFT({
-    name: "Vans Old Skool",
-    size: "9",
-    properties: "Canvas",
-    color: "Blue",
-    brand: "Vans"
-  });
-  mintNFT({
-    name: "Converse Chuck Taylor All Star",
-    size: "8",
-    properties: "Leather",
-    color: "White",
-    brand: "Converse"
-  });
-  mintNFT({
-    name: "Balenciaga Triple S",
-    size: "7",
-    properties: "Mesh",
-    color: "Yellow",
-    brand: "Balenciaga"
-  });
-  listNFTs();
- 
-  getTotalSupply()
+  let nft = {
+    brand: brand,
+    collection: collection,
+    name: name,
+    description: description
+  };
+
+  nftArray.push(nft);
+}
+
+mintNFT("OXGN", "OXGN Collection", "OXGN", "OXGN is a fashion brand designed for all");
+mintNFT("Bench", "Bench Tshirt Collection", "Bench", "Bench is a Philippine retail brand of clothing");
+
+console.log(nftArray); 
